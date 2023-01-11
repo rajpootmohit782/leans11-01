d99d36f58d1a039107ba830d7220ee8aa02171e8
rajpootmohit782 <84062871+rajpootmohit782@users.noreply.github.com>
2023-01-12 00:44:03 +0530

    t8

--- src/App.js
+++ src/App.js
@@ -5,34 +5,48 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18"
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19"
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: "Some Dummy Movie",
+  //     openingText: "This is the opening text of the movie",
+  //     releaseDate: "2021-05-18"
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Some Dummy Movie 2",
+  //     openingText: "This is the second opening text of the movie",
+  //     releaseDate: "2021-05-19"
+  //   }
+  // ];
 
   async function fetchapiMoviesHandle() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    console.log(data);
-    const transFormmovieData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
-    });
-    setMovies(transFormmovieData);
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/filmss/");
+      if (!response.ok) {
+        throw new Error("Something went wrong! ....Retrying");
+      }
+      const data = await response.json();
+      console.log(data);
+
+      const transFormmovieData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovies(transFormmovieData);
+    } catch (error) {
+      setError(error.message);
+      fetchapiMoviesHandle();
+    }
+    setIsLoading(false);
   }
 
   return (
@@ -41,7 +55,12 @@ function App() {
         <button onClick={fetchapiMoviesHandle}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && !error && movies.length === 0 && (
+          <p>To fetch click on Fetch Movies...</p>
+        )}
+        {!isLoading && error && <p>{error}</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
